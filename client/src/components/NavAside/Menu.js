f4c14fff9c35a776410c2a3522c4727d0676078d
zerg41 <zerg41@gmail.com>
2021-12-13 08:35:57 +0300

    fixed: Menu component
    -deleted unused state

--- client/src/components/NavAside/Menu.js
+++ client/src/components/NavAside/Menu.js
@@ -1,10 +1,6 @@
-import {useState} from 'react';
 import MenuItem from './Menu/MenuItem';
 
 function Menu() {
-
-    const [isFirstItemActive, setIsFirstItemActive] = useState(false);
-    const [isSecondItemActive, setIsSecondItemActive] = useState(false);
 
     return(
         <div className="pt-4">
